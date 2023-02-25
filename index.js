951c20f7747ea08e2cea8d0d18576c1bc1edaa57
amorissette26 <124222744+amorissette26@users.noreply.github.com>
2023-02-25 10:42:53 -0500

    added hi/low temp, added humidity and windspeed

--- index.js
+++ index.js
@@ -43,6 +43,16 @@ function displayWeather(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let tempHiElement = document.querySelector("#temp-hi");
+  let tempLowElement = document.querySelector("#temp-lo");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  tempHiElement.innerHTML = Math.round(response.data.main.temp_max);
+  tempLowElement.innerHTML = Math.round(response.data.main.temp_min);
 }
 
 function searchForCity(event) {
